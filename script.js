ea19fb5cdfdcbf691a8130dfd6a9acd78f4672c2
vitavococo <gcastanedalondono@gmail.com>
2024-11-16 10:28:17 -0500

    lista paraSha Vayeira

--- script.js
+++ script.js
@@ -26,6 +26,7 @@ document.addEventListener("DOMContentLoaded", function() {
   });
 });
 
+
 const podcast=document.querySelector("icon-bnt cloud");
 podcast.addEventListener("mouseover", () =>{
     podcast.backgroundColor="blue";
